d0ee85cd7b97e048f9ff932f415feef5fdbb2b15
saurabh <ykings.saurabh@gmail.com>
2020-08-20 21:16:22 +0530

    reorder link list

---
+++ augast2020/Day20_reorder_linkList.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head==null||head.next==null) return;
+        Deque<ListNode> stack = new ArrayDeque<ListNode>();
+        ListNode ptr=head;
+        while (ptr!=null) {
+            stack.push(ptr); ptr=ptr.next;
+        }
+        int cnt=(stack.size()-1)/2;
+        ptr=head;
+        while (cnt-->0) {
+            ListNode top = stack.pop();
+            ListNode tmp = ptr.next;
+            ptr.next=top;
+            top.next=tmp;
+            ptr=tmp;
+        }
+        stack.pop().next=null;
+    }
+}
+
+public class Day20_reorder_linkList{
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+    }
+}
